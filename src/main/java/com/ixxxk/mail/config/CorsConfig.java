7c65aa5d18804308e6846f3a4258ac923f027694
tany <tyongysjcel>
2020-07-02 00:53:55 +0800

    update

--- src/main/java/com/ixxxk/mail/config/CorsConfig.java
+++ src/main/java/com/ixxxk/mail/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.ixxxk.mail.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,27 +13,40 @@ import org.springframework.web.filter.CorsFilter;
  * @createDate: 2020/7/1
  * @version: 1.0.0
  */
+/**
+ * 跨域配置
+ */
 @Configuration
-public class CorsConfig {
+public class CorsConfig {//nohup java -jar mail-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro &
+    // 设置允许跨域的源
+    private static String[] originsVal = new String[]{
+            "127.0.0.1:8080",
+            "localhost:8080",
+            "ixxxk.com",
+            "www.ixxxk.com"
+    };
 
-    @Value("${api.corss.open}")
-    private boolean openCors;
-
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.setAllowCredentials(true);
-        return corsConfiguration;
-    }
-
+    /**
+     * 跨域过滤器
+     *
+     * @return
+     */
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        if(openCors){
-            source.registerCorsConfiguration("/**", buildConfig());
-        }
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        this.addAllowedOrigins(corsConfiguration);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(source);
+    }
+
+    private void addAllowedOrigins(CorsConfiguration corsConfiguration) {
+        for (String origin : originsVal) {
+            corsConfiguration.addAllowedOrigin("http://" + origin);
+            corsConfiguration.addAllowedOrigin("https://" + origin);
+        }
     }
 }
--- src/main/java/com/ixxxk/mail/controller/MailController.java
+++ src/main/java/com/ixxxk/mail/controller/MailController.java
@@ -27,10 +27,19 @@ public class MailController {
     private SendMailService sendMailService;
 
     @PostMapping("/send")
-    public Result<Object> sendMail(@RequestParam(value = "toUser") String toUser, @RequestParam(value = "subject") String subject, @RequestParam(value = "text") String text, HttpServletRequest request) {
+    public Result<Object> sendMail(
+            @RequestParam(value = "toUser") String toUser
+            , @RequestParam(value = "subject") String subject
+            , @RequestParam(value = "text") String text
+            , HttpServletRequest request) {
         log.info("send...............");
         String ipAddr = IpUtil.getIpAddr(request);
-        boolean b = sendMailService.sendMail(toUser, subject, text, ipAddr);
+        boolean b = sendMailService.sendMail("5824519@qq.com", subject+":"+toUser, text, ipAddr);
+        try {
+            sendMailService.sendMail(toUser,"来自www.ixxxk.com的回信","您的消息已收到，感谢您的来信。","");
+        }catch (Exception e){
+            
+        }
         return b ? Result.success() : Result.error("-1", "邮件发送失败，请稍后再试...");
     }
 }
